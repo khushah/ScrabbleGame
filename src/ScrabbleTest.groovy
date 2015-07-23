3bb46eaf368f8271ea42424fe50816508af27028
Kushagra8888 <kushagra8888@gmail.com>
2015-07-23 18:35:02 +0530

    merge master and dev. Create test file boilerplate

---
+++ src/ScrabbleTest.groovy
@@ -0,0 +1,33 @@
+/**
+ * Created by test on 7/23/2015.
+ */
+class ScrabbleTest extends groovy.util.GroovyTestCase {
+    void testSortString() {
+        def x = 1
+        assert x == 1
+    }
+
+    void testAddToHash() {
+
+    }
+
+    void testPopulateDictionary() {
+
+    }
+
+    void testFindWordScore() {
+
+    }
+
+    void testPowerSet() {
+
+    }
+
+    void testFindBestScore() {
+
+    }
+
+    void testMain() {
+
+    }
+}
